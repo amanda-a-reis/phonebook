a1db5fe3272a950fda647b35cc9c13f43769f8c4
amanda <amandareisdw@gmail.com>
2023-04-11 18:59:07 -0300

    Created pages to add to cypress tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -9,7 +10,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Hello World!</div>
+      <nav>
+        <h1>Homepage</h1>
+        <Link href="/about">About</Link>
+      </nav>
     </>
   )
 }
