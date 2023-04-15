fccb388d68c9233e5823625373368c3653d3de6f
amanda <amandareisdw@gmail.com>
2023-04-14 21:18:50 -0300

    Created methods by id - api

--- pages/api/contacts/[id].ts
+++ pages/api/contacts/[id].ts
@@ -27,6 +27,56 @@ async function deleteContact (id: number): Promise<any> {
   })
 }
 
+async function updateContact (id: number, contactData: any): Promise<any> {
+  let contact: any
+  if (contactData.name) {
+    contact = await prisma.contact.update({
+      where: {
+        id
+      },
+      data: {
+        name: contactData.name
+      },
+      include: {
+        phones: true
+      }
+    })
+  }
+  if (contactData.age) {
+    contact = await prisma.contact.update({
+      where: {
+        id
+      },
+      data: {
+        age: contactData.age
+      },
+      include: {
+        phones: true
+      }
+    })
+  }
+  contact = await prisma.contact.findUnique({
+    where: {
+      id
+    },
+    include: {
+      phones: true
+    }
+  })
+  return contact
+}
+
+async function updatePhone (id: number, phoneData: any): Promise<any> {
+  await prisma.phone.update({
+    where: {
+      id
+    },
+    data: {
+      number: phoneData.phone
+    }
+  })
+}
+
 export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse<any>
@@ -45,8 +95,28 @@ export default async function handler (
       res.status(200).end('User deleted.')
       break
     }
+    case 'PUT': {
+      const { id } = req.query
+      const {
+        name,
+        age,
+        phones
+      }: { name: string, age: number, phones: string[] } = req.body
+      const contact = await updateContact(Number(id), { name, age })
+      if (phones) {
+        phones.map(async (phone: any, index: number) => {
+          if (contact.phones[index].number === phone.number) {
+            return true
+          } else {
+            await updatePhone(contact.phones[index].id, { phone })
+          }
+        })
+      }
+      res.status(200).end('Contact edited.')
+      break
+    }
     default:
-      res.setHeader('Allow', ['GET', 'DELETE'])
+      res.setHeader('Allow', ['GET', 'DELETE', 'PUT'])
       res.status(405).end('Method Not Allowed')
   }
 }
