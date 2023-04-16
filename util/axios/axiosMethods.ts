455458f5ddf53ebfbe8972874ee95c7085002c10
amanda <amandareisdw@gmail.com>
2023-04-16 16:29:57 -0300

    Changed some config

--- util/axios/axiosMethods.ts
+++ util/axios/axiosMethods.ts
@@ -1,13 +1,17 @@
 import axios from 'axios'
 import type { Contact, ContactInfo } from '../interfaces'
 
+const config = {
+  headers: { 'Access-Control-Allow-Origin': '*' }
+}
+
 let baseURL: string
 if (process.env.NODE_ENV === 'development') {
   baseURL = 'http://localhost:3000/'
 } else if (process.env.NODE_ENV === 'production') {
   baseURL = 'https://phonebook-beige.vercel.app/'
 } else {
-  baseURL = 'http://localhost:3000/'
+  baseURL = ''
 }
 
 async function axiosPost (data: ContactInfo<number, string[]>): Promise<void> {
@@ -17,7 +21,7 @@ async function axiosPost (data: ContactInfo<number, string[]>): Promise<void> {
     name,
     age,
     phones
-  })
+  }, config)
 }
 
 async function axiosPut (data: ContactInfo<number, string[]>, id: string): Promise<void> {
@@ -27,24 +31,24 @@ async function axiosPut (data: ContactInfo<number, string[]>, id: string): Promi
     name,
     age,
     phones
-  })
+  }, config)
 }
 
 async function axiosGet (): Promise<Contact[]> {
   const URL = `${baseURL}api/contacts`
-  const result: Contact[] = (await axios.get(URL)).data.contacts
+  const result: Contact[] = (await axios.get(URL, config)).data.contacts
   return result
 }
 
 async function axiosGetById (id: string): Promise<Contact> {
   const URL = `${baseURL}api/contacts/${id}`
-  const result: Contact = (await axios.get(URL)).data.contact
+  const result: Contact = (await axios.get(URL, config)).data.contact
   return result
 }
 
 async function axiosDelete (id: string): Promise<void> {
   const URL = `${baseURL}api/contacts/${id}`
-  await axios.delete(URL)
+  await axios.delete(URL, config)
 }
 
 export { axiosPost, axiosPut, axiosGet, axiosGetById, axiosDelete }
