bf33560f8629b60a6aed56742416e810adf060ef
amanda <amandareisdw@gmail.com>
2023-04-16 12:57:39 -0300

    Added trycatch to get logs

--- pages/api/contacts/log.ts
+++ pages/api/contacts/log.ts
@@ -17,8 +17,12 @@ export default async function handler (
   const { method } = req
   switch (method) {
     case 'GET': {
-      const logs = getLogFile(pathFile)
-      res.status(200).end(logs)
+      try {
+        const logs = getLogFile(pathFile)
+        res.status(200).end(logs)
+      } catch (error) {
+        res.status(500).end('No logs file found')
+      }
       break
     }
     default:
