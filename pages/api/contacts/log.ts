c036674b9716ec7e6614127fd9ab13cebc548a12
amanda <amandareisdw@gmail.com>
2023-04-16 19:40:14 -0300

    Added logs to db

--- pages/api/contacts/log.ts
+++ pages/api/contacts/log.ts
@@ -1,14 +1,17 @@
-import fs from 'fs'
-import path from 'path'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/lib/prisma'
 
-function getLogFile (path: string): string {
-  const logs: string = fs.readFileSync(path, 'utf-8')
+// function getLogFile (path: string): string {
+//   const logs: string = fs.readFileSync(path, 'utf-8')
+//   return logs
+// }
+// const rootDir = process.cwd()
+// const pathFile = path.join(rootDir, 'logs.txt')
+
+async function getLogFile (): Promise<any> {
+  const logs = await prisma.log.findMany()
   return logs
 }
-
-const rootDir = process.cwd()
-const pathFile = path.join(rootDir, 'logs.txt')
 
 export default async function handler (
   req: NextApiRequest,
@@ -17,12 +20,9 @@ export default async function handler (
   const { method } = req
   switch (method) {
     case 'GET': {
-      try {
-        const logs = getLogFile(pathFile)
-        res.status(200).end(logs)
-      } catch (error) {
-        res.status(500).end('No logs file found')
-      }
+      const logs = await getLogFile()
+      console.log(logs)
+      res.status(200).json({ logs })
       break
     }
     default:
